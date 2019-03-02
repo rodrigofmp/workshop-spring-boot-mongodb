792be24ec47a3d5b0c15bbfbb4524d6063aec319
Rodrigo Petters <rodrigofmp@gmail.com>
2019-03-02 01:42:57 -0300

    Entidade e Controller User

---
+++ src/main/java/com/rodrigofmp/workshopmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.rodrigofmp.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigofmp.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria", "maria@db.com");
+		User alex = new User("2", "alex", "alex@db.com");
+		List<User> list = new ArrayList<User>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
