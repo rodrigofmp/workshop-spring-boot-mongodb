04b69dc87c1a9d688b8edb04aa2a3c1a8b0bfa55
Rodrigo Petters <rodrigofmp@gmail.com>
2019-03-02 03:11:03 -0300

    Update de usuário

--- src/main/java/com/rodrigofmp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rodrigofmp/workshopmongo/resources/UserResource.java
@@ -51,4 +51,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
